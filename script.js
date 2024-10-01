44e0626a787aefdf63426015e46ac6e9561f4a51
Nhlalo <nhlalonkosi@gmail.com>
2024-10-01 16:55:29 +0200

    Added a function and an event listener
    This will notify the user that he has updated the password but not other.

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const passwordConfirmation = document.querySelector('#confirmPassword');
-function confirmPassword(){
 const password = document.querySelector('#password');
-if(password.value !== ''){
+function confirmPassword(){
+
 if(password.value !== passwordConfirmation.value){
   passwordConfirmation.style['border-color'] = 'red';
 }else{
     passwordConfirmation.style['border-color'] = 'black';
 }
-}
+
 }
 //This will ensure that the password confirmation process continues with every character entered.
-passwordConfirmation.addEventListener('keyup', confirmPassword)
+passwordConfirmation.addEventListener('keyup', confirmPassword);
+
+
+/*This will notify the user that the passwords do not match, when the user has entered the correct password on the password confirmation input but then decides to change the passsword on the password input but not on the password confirmation input*/
+function checkPasswordNotChanged(){
+  if(passwordConfirmation.value !== ''){
+  if(password.value !== passwordConfirmation.value){
+    passwordConfirmation.style['border-color'] = 'red';
+  }else{
+      passwordConfirmation.style['border-color'] = 'black';
+  }
+}
+  
+  }
+
+  password.addEventListener('keyup',checkPasswordNotChanged);
 
